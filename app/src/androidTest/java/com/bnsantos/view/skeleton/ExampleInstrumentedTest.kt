f331a7e3847a2e720ab1955db07abebee8dc36b6
Bruno Santos <brunonascimentosantos@gmail.com>
2017-06-09 14:50:31 -0300

    initial commit

---
+++ app/src/androidTest/java/com/bnsantos/view/skeleton/ExampleInstrumentedTest.kt
@@ -0,0 +1,25 @@
+package com.bnsantos.view.skeleton
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    @Throws(Exception::class)
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.bnsantos.view.skeleton", appContext.packageName)
+    }
+}
